5a40b44eaaa4570b721bb9e753c7e56e3c0cbdd2
Ziad Abouelfarah <ziadabouelfarah2@gmail.com>
2018-07-15 12:50:38 +0100

    Interface you can make it like you want

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from product import *
 from users import *
-#import time
 
-h = Product()
-#l = User("ziad","????")
-#there is problem in user shit
-k = Admin()
 
-#h.addProduct("chemise 1/2",80.00) #work
-#h.sell("chemise 1/2",90.00) 
-h.sell("chemise",150.00) 
-#h.mainPrice("chemise 1/2") 
-#h.delete_product("chemise 1/2")
+#Just The Interface you can make it like you want
